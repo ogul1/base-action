1f974fd1c2c4f76338be470b98c961f92b80926a
Emirhan Oğul <emirhan.ogul@metu.edu.tr>
2024-07-27 16:26:54 +0300

    test step end 2

--- utils.py
+++ utils.py
@@ -80,6 +80,10 @@ def modify_file_content(yaml_file_content: str, python_version: str) -> str:
 
         line_indent = get_indent(line)
 
+        if line_indent <= steps_indent and line.lstrip()[0] != '-':  # steps end
+            # add code to collect the logs
+            modified_file = add_push_results_to_another_repository(modified_file)
+
         if "steps" in line:  # steps start
             steps_indent = line_indent
             indent = 0
@@ -88,10 +92,6 @@ def modify_file_content(yaml_file_content: str, python_version: str) -> str:
                     indent = get_indent(initial_file[j])
                     break
             modified_file = add_set_up_python_and_dependencies(modified_file, indent, python_version)
-
-        if line_indent <= steps_indent and line.lstrip()[0] != '-':  # steps end
-            # add code to collect the logs
-            modified_file = add_push_results_to_another_repository(modified_file)
 
     print(modified_file)
 
